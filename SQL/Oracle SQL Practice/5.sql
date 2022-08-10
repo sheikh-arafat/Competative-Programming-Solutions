57d78485bfecc2fa793903278538e1de70985fb5
Sheikh Mohammad Arafat <sheikh.mohammad.arafat@g.bracu.ac.bd>
2022-08-11 00:03:31 +0600

    Completed

--- SQL/Oracle SQL Practice/5.sql
+++ SQL/Oracle SQL Practice/5.sql
@@ -1,9 +1,10 @@
 /*
 5. Display manager information and how many subordinates they manage (count)
-Partial Ans:
+Ans:
 */
-SELECT DISTINCT d.department_id, COUNT(e.employee_id) - 1 AS Subordinates, d.manager_id
-FROM hr.departments d
-INNER JOIN hr.employees e
-ON d.department_id = e.department_id
-GROUP BY d.department_id, d.manager_id;
+select m.employee_id, m.first_name, m.last_name , count(e.employee_id) subordinates 
+from hr.employees e 
+join hr.employees m 
+on e.manager_id =  m.employee_id 
+group by m.employee_id,m.first_name, m.last_name 
+order by m.employee_id;
